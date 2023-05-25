0361248c62495bac00310b3b055aae6fc03df7e5
Sumukh Prasad <sumukhprasad@icloud.com>
2023-05-25 06:53:07 +0530

    Added base

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,44 @@
 <script>
-	export let name;
+	const { ILTEngine } = require("./libilt/indic-language-transliteration/src/engine");
+	const kannada = require('./libilt/indic-language-transliteration/src/schemes/kannada.js').scheme;
+	const devanagari = require('./libilt/indic-language-transliteration/src/schemes/devanagari.js').scheme;
+
+	var engine = new ILTEngine();	
+
+	engine._loadScheme('kannada', kannada, false)
+	engine._loadScheme('devanagari', devanagari, false)
+
+	
+
+	function updateAllTAs(t) {
+	}
 </script>
 
+<head>
+	<title>ILT-GUI</title>
+</head>
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Indic Language Transliteration</h1>
+	<a href="https://github.com/SumukhPrasad/indic-language-transliteration">github.com/SumukhPrasad/indic-language-transliteration</a>
+	<table>
+		<tr>
+			<td>
+				<select id="sch1">
+				</select>
+			</td>
+			<td>
+				<select id="sch2">
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td><textarea id="sch1" on:keyup={updateAllTAs}></textarea></td>
+			<td><textarea id="sch2" on:keyup={updateAllTAs}></textarea></td>
+		</tr>
+	</table>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+	
 </style>
